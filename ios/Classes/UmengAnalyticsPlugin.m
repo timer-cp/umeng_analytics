35cd3d3ef61a210898382f6b817bc80ca5d2fe36
timer <18201085018@163.com>
2021-01-21 12:22:00 +0800

    feat(init plugin): init

---
+++ ios/Classes/UmengAnalyticsPlugin.m
@@ -0,0 +1,20 @@
+#import "UmengAnalyticsPlugin.h"
+
+@implementation UmengAnalyticsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"umeng_analytics"
+            binaryMessenger:[registrar messenger]];
+  UmengAnalyticsPlugin* instance = [[UmengAnalyticsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
